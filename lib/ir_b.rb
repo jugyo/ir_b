464e1a9968ffb800336ca3950a8ed7a911af34ed
jugyo <jugyo.org@gmail.com>
2010-10-08 23:00:26 +0900

    printing current line

--- lib/ir_b.rb
+++ lib/ir_b.rb
@@ -27,6 +27,22 @@ end
 module IrB
   class << self
     def -(_binding)
+      file = _binding.eval '__FILE__'
+      ir_b_line = _binding.eval '__LINE__'
+      puts "#{file}:#{ir_b_line} \e[90mtxmt://open/?url=file://#{ File.expand_path file }&line=#{ ir_b_line }\e[0m"
+
+      File.open(file).each_with_index do |line, index|
+        line_n = index + 1
+        next unless line_n > (ir_b_line - 6)
+        break if line_n > (ir_b_line + 5)
+        if line_n == ir_b_line
+          color = "\e[100m"
+        else
+          color = "\e[90m"
+        end
+        puts color + "#{line_n.to_s.rjust(5)}: #{line.strip} \e[0m"
+      end
+
       IRB.start_session(_binding)
     end
   end
